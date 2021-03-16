a8a3764abe318cb8b8a1e73c157d12ec7de61a94
Pranoti <pranoti110@gmail.com>
2021-03-16 20:08:41 +0530

    Adding file

---
+++ DOS/test.sql
@@ -0,0 +1,6 @@
+create table studentData(Name text, Roll_no int primary key, Address text, Mobile_number int, PAN_Number varchar(30));
+insert into studentData values('Alvaro Morte',18111,'215 Spain',789654678,'ABC12345');
+insert into studentData values('Nairobi',18211,'512 old lane new york',897654618,'PQR12345');
+insert into studentData values('Tokyo',18311,'15 Yemen road Yemen',976454678,'ABC9874');
+insert into studentData values('Rio',18411,'12 Rio de janeiro',87564678,'DEF1256');
+insert into studentData values('Raquel',18811,'park street new zealand',9659654678,'LMH12855');
